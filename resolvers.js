930c9ea3ed5347a6e5e8680f97d3b97f0f930fd4
Cyndi Chin <cgc95@cornell.edu>
2021-11-18 10:16:04 -0500

    Add move and clean up schema

--- resolvers.js
+++ resolvers.js
@@ -1,19 +1,24 @@
 const resolvers = {
   Query: {
-    getAllPokemon: async (_, __, { dataSources }) => {
-      const { results } = await dataSources.pokemonAPI.getPokemons();
-      return results;
+    getAllPokemon: (_, __, { dataSources }) => {
+      // Removing async / await avoids null values - need to investigate
+      // const { results } = await dataSources.pokemonAPI.getPokemons();
+      return dataSources.pokemonAPI.getPokemons();
     },
-    pokemon: async (_, {id}, { dataSources }) => {
-      const items = await dataSources.pokemonAPI.getPokemon(id);
-      return items;
+    pokemon: (_, {id}, { dataSources }) => {
+      // const items = await dataSources.pokemonAPI.getPokemon(id);
+      return dataSources.pokemonAPI.getPokemon(id);
+    },
+    getAllMoves: (_, __, { dataSources }) => {
+      return dataSources.pokemonAPI.getMoves();
     },
   },
   // Pokemon: {
-  //   stats: async (_, { id }, { dataSources }) => {
-  //     const { results } = await dataSources.pokemonAPI.getStat(id);
-  //     return results;
-  //   },
+    // The id seems to be for the ability and stat, not the id of the pokemon
+    // stats: async (_, { id }, { dataSources }) => {
+    //   const { results } = await dataSources.pokemonAPI.getStat(id);
+    //   return results;
+    // },
   // },
 };
 
--- schema.js
+++ schema.js
@@ -3,12 +3,32 @@ const { gql } = require("apollo-server");
 const typeDefs = gql`
 
   type Query {
-    getAllPokemon: [Pokemon!]!
+    getAllPokemon: ResourceList
     pokemon(id: Int): Pokemon
     # pokemonStat(id: Int): Pokemon!
+    getAllMoves: ResourceList
+  }
+
+  type ResourceList {
+    "The total number of resources available from this API."
+    count: Int
+    "The URL for the next page in the list."
+		next: String
+    "The URL for the previous page in the list."
+		previous: String
+    "A list of named API resources."
+		results: [EndpointResult]
+  }
+
+  type EndpointResult {
+    "The name of the referenced resource."
+    name: String
+    "The URL of the referenced resource."
+    url: String
   }
 
   type Pokemon {
+    "The pokemon's id"
     id: Int
     "The pokemon's name"
     name: String!
@@ -18,27 +38,41 @@ const typeDefs = gql`
     stats: [StatObj]
     "List of abilities this pokemon can have"
     abilities: [AbilityObj]
+    moves: [Move]
   }
 
   "Stat determines certain aspects of battles"
   type StatObj {
     base_stat: Int
     effort: Int
-    stat: Stat
-  }
-
-  type Stat {
-    name: String
+    stat: Name
   }
 
   "Pokemon can have multiple possible abilities but can only have one ability at a time"
   type AbilityObj {
-    ability: Ability
+    ability: Name
     is_hidden: Boolean
     slot: Int
   }
 
-  type Ability {
+  type Move {
+    "The move the Pokémon can learn."
+    move: Name
+    "The details of the version in which the Pokémon can learn the move."
+    version_group_details: [PokemonMoveVersion]
+  }
+
+  type PokemonMoveVersion {
+    "The method by which the move is learned."
+    move_learn_method: Name
+    "The version group in which the move is learned."
+    version_group: Name
+    "The minimum level to learn the move."
+    level_learned_at: Int
+  }
+
+  "Reusable name type"
+  type Name {
     name: String
   }
 `;
